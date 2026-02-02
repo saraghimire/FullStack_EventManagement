d0adc7078fee4f8ed32b1fb2ad651e4a8c10086d
saraghimire <saraghimirekshetri@gmail.com>
2026-02-02 10:57:53 +0545

    done

--- config/db.php
+++ config/db.php
@@ -2,13 +2,13 @@
 class Database {
     public static function connect() {
         $host = 'localhost';
-        $db   = 'event_db';
-        $user = 'root'; 
-        $pass = ''; 
+        // $db   = 'event_db';
+        // $user = 'root'; 
+        // $pass = ''; 
 
-        //$db   = 'np02cs4s250012';
-        //$user = 'np02cs4s250012'; 
-        //$pass = 'DajF62ixgT'; 
+        $db   = 'np02cs4s250012';
+        $user = 'np02cs4s250012'; 
+        $pass = 'DajF62ixgT'; 
 
         try {
             $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass, [
